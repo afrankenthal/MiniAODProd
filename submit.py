f4d9e8140bca1e01f93b95335770cd2cf476c04e
SiewYan <siew.yan.hoh@cern.ch>
2017-08-27 14:29:04 -0500

    added parameter RequestMemory = 3000

--- submit.py
+++ submit.py
@@ -11,22 +11,23 @@ njobs = argv[2]
 
 classad='''
 universe = vanilla                                                                                                                                                                       
-executable = {0}/exec.sh                                                                                                                                                                                   
-should_transfer_files = YES                                                                                                                                                                                
-when_to_transfer_output = ON_EXIT                                                                                                                                                                          
-transfer_input_files = {0}/submit.tgz,{0}/x509up                                                                                                                                                           
-transfer_output_files = ""                                                                                                                                                                                 
-input = /dev/null                                                                                                                                                                                          
-output = {1}/$(Cluster)_$(Process).out                                                                                                                                                                     
-error = {1}/$(Cluster)_$(Process).err                                                                                                                                                                      
-log = {1}/$(Cluster)_$(Process).log                                                                                                                                                                        
-rank = Mips                                                                                                                                                                                                
-arguments = $(Process)                                                                                                                                                                                     
-use_x509userproxy = True                                                                                                                                                                                   
-x509userproxy = /tmp/x509up_u{2}                                                                                                                                                                           
-#on_exit_hold = (ExitBySignal == True) || (ExitCode != 0)                                                                                                                                                  
+executable = {0}/exec.sh                                                                                                                                       
+should_transfer_files = YES                                                                                                                               
+when_to_transfer_output = ON_EXIT                                                                                                                                 
+transfer_input_files = {0}/submit.tgz,{0}/x509up                                                                                                                                              
+transfer_output_files = ""                                                                                                                                                                      
+input = /dev/null                                                                                                                                                                           
+output = {1}/$(Cluster)_$(Process).out                                                                                                                                                        
+error = {1}/$(Cluster)_$(Process).err                                                                                                                                                          
+log = {1}/$(Cluster)_$(Process).log                                                                                                                                                 
+rank = Mips                                                                                                                                                                                 
+RequestMemory = 3000
+arguments = $(Process)                                                                                                                                                                     
+use_x509userproxy = True                                                                                                                                                                        
+x509userproxy = /tmp/x509up_u{2}                                                                                                                                                        
+#on_exit_hold = (ExitBySignal == True) || (ExitCode != 0)                                                                                                                                      
 +AccountingGroup = "analysis.shoh"  
-+AcctGroup = "analysis"                                                                                                                                                                                    
++AcctGroup = "analysis"                                                                                                                                                                            
 +ProjectName = "DarkMatterSimulation"                                                                                                                                                                    
 queue {3}  
 '''.format(workpath,logpath,uid,njobs)
