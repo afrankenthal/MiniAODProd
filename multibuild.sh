4912e3545165fd8657b032f7a943c6e21a9ec20b
SiewYan <siew.yan.hoh@cern.ch>
2017-08-27 14:30:45 -0500

    added auxiliary file for bunch submission

---
+++ multibuild.sh
@@ -0,0 +1,29 @@
+#!/bin/bash                                                                                        
+                                                                                                                                                                                    
+eos="/store/user/shoh/gridpacks/DiZprime/Vector"
+
+njob="2"
+
+if [ -e multisubmit.sh ];then
+rm multisubmit.sh
+fi
+
+echo "#!/bin/bash" > multisubmit.sh
+
+for file in `(ls ${eos})`
+do
+xrdcp root://cmseos.fnal.gov//${eos}/${file} inputs/
+
+fbname=$(basename ${file} _tarball.tar.xz)
+
+scp inputs/tmp_hadronizer.py inputs/${fbname}_hadronizer.py
+echo "./auto ${fbname}"
+./auto ${fbname}
+echo "python submit.py work_${fbname} ${njob}" >> multisubmit.sh
+
+rm inputs/${fbname}_hadronizer.py
+rm inputs/${file}
+
+done
+chmod +x multisubmit.sh
+echo "multisubmit is prepared"
