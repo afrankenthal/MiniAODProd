5859b5479502b78c2f8e02dd90ab6464db1975bc
SiewYan <siew.yan.hoh@cern.ch>
2017-08-27 14:29:36 -0500

    update

--- submit/inputs.sh
+++ submit/inputs.sh
@@ -1,5 +1,5 @@
-TARBALL=Vector_Zprime_NLO_Mphi-300_Mchi-1_gSM-0p25_gDM-1p0_13TeV-madgraph_tarball.tar.xz
-HADRONIZER=Vector_Zprime_NLO_Mphi-300_Mchi-1_gSM-0p25_gDM-1p0_13TeV-madgraph_hadronizer.py
-PROCESS=Vector_Zprime_NLO_Mphi-300_Mchi-1_gSM-0p25_gDM-1p0_13TeV-madgraph
+TARBALL=Vector_Zprime_NLO_Mphi-750_Mchi-600_gSM-0p25_gDM-1p0_13TeV-madgraph_tarball.tar.xz
+HADRONIZER=Vector_Zprime_NLO_Mphi-750_Mchi-600_gSM-0p25_gDM-1p0_13TeV-madgraph_hadronizer.py
+PROCESS=Vector_Zprime_NLO_Mphi-750_Mchi-600_gSM-0p25_gDM-1p0_13TeV-madgraph
 USERNAME=shoh
 MERGE=0
