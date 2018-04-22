8ac357956fa9e4d3f2e67cb182775df932dfcc7c
Weinan Si <siweinan@gmail.com>
2018-04-22 12:53:40 -0500

    setup

---
+++ submit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+voms-proxy-init -voms cms -valid 192:00
+
+./submit.py SIDMmumu_Mps-200_MZp-1p2_ctau-0p012.tar.xz    10
+#./submit.py SIDMmumu_Mps-200_MZp-1p2_ctau-0p12.tar.xz     10
+#./submit.py SIDMmumu_Mps-200_MZp-1p2_ctau-0p6.tar.xz      10
+#./submit.py SIDMmumu_Mps-200_MZp-1p2_ctau-1p2.tar.xz      10
+#./submit.py SIDMmumu_Mps-200_MZp-1p2_ctau-1p20e-03.tar.xz 10
+#./submit.py SIDMmumu_Mps-200_MZp-1p2_ctau-3p6.tar.xz      10
